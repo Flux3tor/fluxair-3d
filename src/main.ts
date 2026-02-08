f71b68c1065211e0dfcbcd25831b8b4b966cfccb
Flux3tor <tusharsinha199@gmail.com>
2026-02-08 16:39:14 +0530

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,53 @@
+import { Hands } from "@mediapipe/hands";
+import { Camera } from "@mediapipe/camera_utils";
+import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
+
+const videoElement = document.createElement("video");
+videoElement.style.display = "none";
+document.body.appendChild(videoElement);
+
+const canvasElement = document.createElement("canvas");
+const canvasCtx = canvasElement.getContext("2d")!;
+document.body.appendChild(canvasElement);
+
+canvasElement.width = window.innerWidth;
+canvasElement.height = window.innerHeight;
+
+const hands = new Hands({
+  locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+  },
+});
+
+hands.setOptions({
+  maxNumHands: 1,
+  modelComplexity: 1,
+  minDetectionConfidence: 0.7,
+  minTrackingConfidence: 0.7,
+});
+
+hands.onResults((results) => {
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+  canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+
+  if (results.multiHandLandmarks) {
+    for (const landmarks of results.multiHandLandmarks) {
+      drawConnectors(canvasCtx, landmarks, Hands.HAND_CONNECTIONS);
+      drawLandmarks(canvasCtx, landmarks);
+    }
+  }
+
+  canvasCtx.restore();
+});
+
+const camera = new Camera(videoElement, {
+  onFrame: async () => {
+    await hands.send({ image: videoElement });
+  },
+  width: 1280,
+  height: 720,
+});
+
+camera.start();
